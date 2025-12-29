bfddc6ec48b000148cd9d1e3f8a46addc66cb936
Mahesh <mahesh@cognizant.com>
2025-12-29 18:58:52 +0530

    Initial commit

---
+++ src/test/java/com/mylearning/SpringOAuth/SpringOAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylearning.SpringOAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
